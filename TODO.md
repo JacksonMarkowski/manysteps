34d0ad5ea3c7474a81f6774518a9994626374c5a
Jackson Markowski <markowski.jackson@gmail.com>
2021-09-04 19:22:30 -0600

    Added more required setup options

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# ToDo
+### ToDo
+- [ ] Proper handling for step dependencies creating a cycle
+
+### Done
--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+print(find_packages(include=["manysteps", "manysteps.*"]))
 
 setup(
-    name="manysteps",
-    version="0.0.1",
-    python_requires='>=3.6',
-    url="https://github.com/JacksonMarkowski/manysteps",
-    description="A very insightful description",
     author="Jackson Markowski",
-    packages = ['manysteps']
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="A short description",
+    # install_requires=requirements,
+    license="MIT license",
+    # long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords="manysteps",
+    name="manysteps",
+    packages=find_packages(include=["manysteps", "manysteps.*"]),
+    package_data={"manysteps": ["py.typed"]},
+    # test_suite='tests',
+    # tests_require=test_requirements,
+    url="https://github.com/JacksonMarkowski/manysteps",
+    version="0.0.1",
+    zip_safe=False,
 )
